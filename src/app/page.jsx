fc117a8522495f328261f96f07f07104afe97f78
Liz Robson <lizziearobson@gmail.com>
2024-01-17 15:40:27 +0000

    setting up tailwind and supabase auth

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import Login from './login/page'
+
+export default function Home() {
+
+  const cookieStore = cookies()
+  const supabase = createServerComponentClient({ cookies: () => cookieStore })
+
+  return (
+    <Login />
+  )
+}
