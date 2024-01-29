5470f0b6fd8b6cf58094ac81ae01ee158e25bb40
Liz Robson <lizziearobson@gmail.com>
2024-01-29 20:33:22 +0000

    setting up data fetch

--- src/app/pairing/page.jsx
+++ src/app/pairing/page.jsx
@@ -1,13 +1,22 @@
+import supabase from '../../../utils/supabase';
+
+export async function getServerSideProps() {
+    const {data, error} = await supabase.from('pairing').select('*');
+    return {
+        props: {
+            data,
+            error
+        }
+    }
+}
 
 
+export default function Pairing( {data}) {
 
-import react from 'react';
-
-
-export default function Pairing() {
     return (
         <div>
             <h1 className="bg-white f-white">Pairing</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
     )
 }
