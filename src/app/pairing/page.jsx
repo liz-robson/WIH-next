75a47f699976e46941e5a19ee12ff62e53229658
Liz Robson <lizziearobson@gmail.com>
2024-01-29 21:41:09 +0000

    still trying to fetch data

--- src/app/pairing/page.jsx
+++ src/app/pairing/page.jsx
@@ -1,17 +1,15 @@
-import supabase from '../../../utils/supabase';
-
-async function getData() {
-    const {data} = await supabase.from('pairing').select('*');
-    return {data}
-    };
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
 export default async function Pairing() {
-    const {data} = await getData();
+    const cookieStore = cookies();
+    const supabase = createServerComponentClient({ cookies: () => cookieStore });
+    const { data: pairing } = await supabase.from('pairing').select('*');
+    console.log(pairing);
 
     return (
         <div>
             <h1 className="bg-white f-white">Pairing</h1>
-            <pre className="bg-white f-white">{JSON.stringify(data, null, 2)}</pre>
         </div>
     )
 }
